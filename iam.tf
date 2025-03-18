0fe855c3df5add8794b5499e5af4f1aa76db530d
Adam Breon <90728871+relyabreon@users.noreply.github.com>
2025-03-18 10:52:53 -0700

    RD-25647 Grant sts:TagSession permission to main role (#6)
    * RD-25647 Give main role sts:TagSession permission
    
    * RD-25647 Bump tag (version) to 0.3.1

--- iam.tf
+++ iam.tf
@@ -76,7 +76,7 @@ resource "aws_iam_role_policy" "main" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action   = "sts:AssumeRole",
+        Action   = ["sts:AssumeRole", "sts:TagSession"]
         Effect   = "Allow",
         Resource = local.assumable_roles
       }
